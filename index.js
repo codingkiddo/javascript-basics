0eddad07f0daf9f594b207563d005c46f3fa5f3b
Vinod Kumar <vinodkumar@Vinods-MacBook-Air.local>
2022-03-05 17:31:24 +0530

    pending

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log(document);
+
+
+var element = document.getElementById("first");
+
+
+setTimeout(() => {
+    var i, li,
+	frag = document.createDocumentFragment(),
+	ul = frag.appendChild(document.createElement('ul'));
+
+    for (i = 1; i <= 3; i++) {
+        li = ul.appendChild(document.createElement('li'));
+        li.textContent = 'item ' + i;
+    }
+
+    document.body.appendChild(frag);
+   
+}, 10000);
+
+
+// console.log(element.style.color);
