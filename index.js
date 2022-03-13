0d47a37b956170ecd512b7b2761c17f69b56890d
Vinod Kumar <vinodkumar@Vinods-MacBook-Air.local>
2022-03-13 20:03:51 +0530

    pending

--- index.js
+++ index.js
@@ -1,153 +1,117 @@
 
 
+// console.log(constructor.__proto__.create(null));
+// console.log(constructor.prototype.create(null));
+console.log(Window.prototype);
+console.log(window);
+console.log(constructor);
+console.log(constructor.prototype.constructor.prototype.__proto__.__proto__.__proto__.constructor.__proto__.__proto__.constructor.__proto__.__proto__);
+console.log(constructor == window);
 
-// function foo(a) {
-//     console.log(a+b);
-//     b=a;
+
+
+
+
+console.log(Object);
+console.log(Object.toString());
+console.log(Object.prototype);
+
+console.log(Object.prototype.__proto__);
+console.log(Object.prototype.constructor);
+console.log(Object.prototype.constructor.prototype);
+
+console.log(Object.constructor);
+console.log(Object.constructor.__proto__);
+console.log(Object.constructor.__proto__.__proto__);
+console.log(Object.constructor.__proto__.__proto__.constructor);
+console.log(Object.constructor.prototype);
+console.log(Object.constructor.prototype.prototype);
+
+
+
+
+function hello() {}
+
+var obj = {
+    a: 2
+};
+
+
+
+console.log(Object.__proto__.__proto__);
+console.log(obj.__proto__);
+console.log(obj.prototype);
+
+console.log(Object.__proto__.__proto__ == obj.__proto__);
+console.log(Function.__proto__);
+console.log(Function.prototype);
+console.log(Function.prototype == Function.__proto__);
+console.log(Function.prototype.__proto__.constructor);
+console.log(Function.prototype.__proto__.constructor.create(null));
+console.log(hello.__proto__.__proto__.__proto__);
+console.log(hello.__proto__.__proto__);
+console.log(hello.__proto__.constructor.__proto__.__proto__.constructor.create(null));
+console.log(Function.prototype == hello.__proto__);
+console.log(hello.__proto__ == Function.__proto__);
+
+
+
+
+
+var arr = [];
+console.log(Array);
+console.log(Array.prototype);
+console.log(Array.prototype.__proto__);
+
+console.log(Array.prototype == arr.__proto__);
+console.log(arr.__proto__.__proto__);
+console.log(arr.__proto__.__proto__.__proto__);
+console.log(Array.prototype.__proto__ == Object.prototype);
+
+
+
+
+// console.log(Object.hasOwnProperty("a"));
+// console.log(obj.hasOwnProperty("a"));
+
+
+
+// function identify() {
+//     console.log(this);
 // }
 
-// foo(2);
+// var me = {
+//     name: "Vinod"
+// }
 
+// var you = {
+//     name: "Kumar"
+// }
 
-// console.log(eval("var b = 3;"));
-// console.log(b);
-
-
+// identify.apply(me);
+// identify.apply(you);
 
 
 // function foo() {
-//     "use strict";
-//     console.log(a);
+//     foo.count = 4; // `foo` refers to itself
+//     console.log(foo.count);
 // }
-// var a = 2
 // foo();
+// console.log("foo.count: ", foo.count);
+// console.log("foo.name: ", foo.name);
 
 
-// function foo(str) {
-//     "use strict";
-//     eval(str);
-//     console.log(a);
+// var a = 1;
+
+// function foo() {
+//     console.log( this.a );
 // }
 
-// foo("var a = 2");
-
-// var obj = {};
-// function a(b,c) {}
-
-
-// console.log(Object.getPrototypeOf(Math.max) == Function.prototype);
-
-// console.log(Object.getPrototypeOf(Function.prototype) == Object.prototype);
-// console.log(Object.getPrototypeOf(Array.prototype) == Object.prototype);
-
-// console.log(Object.prototype);
-// console.log(obj.__proto__);
-// console.log(obj.prototype);
-// console.log(obj.__proto__ === Object.prototype);
-
-// // console.log(Object.getPrototypeOf(Object.prototype));
-
-
-
-// var anotherObject = {
-//     a: 2
+// var obj = {
+//     a: 2,
+//     foo: foo
 // };
-// // create an object linked to `anotherObject`
-// var myObject = Object.create( anotherObject );
-// console.log(anotherObject);
-// console.log(myObject); // 2
 
-// for (var k in anotherObject) {
-//     console.log("found: " + k);
-// }
+// foo();
+// obj.foo(); // 2
 
-// console.log("a" in myObject);
-
-console.log(" ============================================== ");
-console.log(Object);
-console.log(Object.prototype);
-console.log(" ============================================== ");
-
-console.log(" ============================================== ");
-console.log(Object.hasOwnProperty("a"));
-console.log(Object.prototype.hasOwnProperty("a"));
-console.log(" ============================================== ");
-
-console.log(" ============================================== ");
-console.log(Object.create({}));
-// console.log(Object.prototype.create({}));
-console.log(" ============================================== ");
-
-console.log(" ============================================== ");
-var obj = {a:100};
-console.log(obj);
-console.log(obj.__proto__);
-console.log(obj.prototype);
-
-console.log(Object.prototype.__proto__);
-console.log(Object.prototype);
-console.log(Function.prototype.__proto__);
-console.log(Function.prototype);
-console.log(Object.prototype == Function.prototype.__proto__);
-console.log(" ============================================== ");
-
-function hello(name) {
-    console.log("Hello ", name);
-}
-
-console.log(hello.name);
-console.log(hello.a);
-
-console.log(hello.prototype);
-console.log(hello.prototype.__proto__);
-hello.prototype.__proto__ = obj;
-console.log(hello.prototype.__proto__);
-console.log(hello.a);
-
-
-console.log(" ============================================== ");
-function Foo() {
-// ...
-}
-var a = new Foo();
-
-
-console.log(a.length);
-console.log(Function.length);
-console.log(Foo.prototype);
-console.log(Foo.__proto__);
-console.log(Foo.prototype.__proto__);
-
-console.log(a.prototype);
-console.log(a.__proto__);
-console.log(a.__proto__.__proto__);
-console.log(a.__proto__ == Foo.prototype);
-
-
-
-
-var obj = { a: 100 }
-// console.log(obj.create({}));
-console.log(obj);
-console.log(obj.__proto__);
-console.log(obj.prototype);
-console.log(obj.__proto__ == obj.prototype);
-var obj2 = new Object();
-console.log(obj2.__proto__);
-console.log(obj2.prototype);
-console.log(obj2.__proto__ == obj2.prototype);
-
-
-
-// Closure
-
-function main() {
-    var name = "Vinod";
-
-    return function() {
-        console.log("Hello ", name);
-    }
-}
-
-var hello = main();
-hello();
