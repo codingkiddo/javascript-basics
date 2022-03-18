269d6fe08d75b407ba7d1ef54cccbdadc254eca1
Vinod Kumar <vinodkumar@Vinods-MacBook-Air.local>
2022-03-18 10:37:12 +0530

    pending

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log(document);
+var start = Date.now();
+
+function incrementX(obj) {
+    return 1 + obj.x;
+}
+
+setTimeout(function () {
+    console.log("Done with timeout !!"); 
+}, 5000);
+
+for (var i=0; i<1000000000; i++) {}
+
+console.log(incrementX({ x: 42 }));  
+
+var end = Date.now();
+
+console.log("Time : ", (end-start)/1000);  
