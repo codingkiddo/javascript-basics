203dfb93c195f05e61122f18273414fbeef2df69
Vinod Kumar <vinodkumar@Vinods-MacBook-Air.local>
2022-03-25 10:48:27 +0530

    pending

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof NaN);
+console.log(NaN == NaN);
+console.log(NaN === NaN);
+console.log(0 == 0);
+console.log(+0 == -0);
+console.log(-0 == +0);
+
+console.log(+0);
+console.log(-0);
+console.log(!+0);
+console.log(!-0);
+console.log(!(+0));
+console.log(!(-0));
 
 
+console.log(true == +0);
+console.log(+0 == true);
+console.log(typeof +0);
+
+console.log(typeof []);
+console.log(typeof +[]);
 // console.log(typeof []);
 
 // console.log(null == undefined);
@@ -13,11 +34,21 @@
 // console.log("document.all: ", document.all.__proto__.__proto__.prototype);
 
 
-console.log("Object: ", Object);
-console.log("Object.prototype: ", Object.prototype);
-console.log("Object.__proto__: ", Object.__proto__);
+// console.log("Object: ", Object);
+// console.log("Object.prototype: ", Object.prototype);
+// console.log("Object.__proto__: ", Object.__proto__);
 
-let obj = new Object();
-console.log("obj: ", obj);
-console.log("obj.prototype: ", obj.prototype);
-console.log("obj.__proto__: ", obj.__proto__);
+// let obj = new Object();
+// console.log("obj: ", obj);
+// console.log("obj.prototype: ", obj.prototype);
+// console.log("obj.__proto__: ", obj.__proto__);
+
+
+
+console.log(typeof []);
+console.log([]);
+console.log(![]);
+console.log(-[]);
+console.log(+[]);
+console.log([] == []);
+console.log([] === []);
